09d4446a481b3967ff213fdb4d63bb2c1ef13ec0
chhzh123 <982270930@qq.com>
2020-01-02 21:45:42 +0800

    [frontend] fix icon

--- aido/urls.py
+++ aido/urls.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 from aido.views import ChatterBotAppView, ChatterBotApiView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^$', ChatterBotAppView.as_view(), name='main'),
     url(r'^admin/', admin.site.urls, name='admin'),
     url(r'^api/chatterbot/', ChatterBotApiView.as_view(), name='chatterbot'),
+    url(r'^favicon.ico$',RedirectView.as_view(url=r'static/favicon.ico')),
 ]
 
 # https://overiq.com/django-1-10/handling-media-files-in-django/
