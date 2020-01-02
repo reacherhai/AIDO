98be955c6fc4bff84952114110326e95043b6190
chhzh123 <982270930@qq.com>
2020-01-02 14:58:11 +0800

    [frontend] Fix bugs in todo

--- aido/delCmdCh.py
+++ aido/delCmdCh.py
@@ -2,5 +2,8 @@
 import re
 
 def deal_control_char(s):
-	temp=re.sub('[\x00-\x09|\x0b-\x0c|\x0e-\x1f]','',s)
-	return temp
+	s = re.sub(r"\x1b\[33m","",s)
+	s = re.sub(r"\x1b\[0m","",s)
+	s = re.sub(r"\x1b\[36m","",s)
+	s = re.sub(r'[\x00-\x09|\x0b-\x0c|\x0e-\x1f]','',s)
+	return s
